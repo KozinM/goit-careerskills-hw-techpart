d18bb7b319823d0cbf22106861cad1ca56dfc9cc
Maxim <31155487+KozinM@users.noreply.github.com>
2023-06-07 12:45:58 +0200

    Most done MVP

--- src/components/UsersList/UserCard/UserCard.js
+++ src/components/UsersList/UserCard/UserCard.js
@@ -4,12 +4,12 @@ import pictureQuestion from './pictureQuestion.png';
 import { userFollowToggle } from 'redux/usersSlice';
 import { useDispatch } from 'react-redux';
 
-export const UserCard = ({ id, username, followers, avatar, status }) => {
+export const UserCard = ({ id, tweets, followers, avatar, status }) => {
 
   const dispatch = useDispatch();
 
 const handleFollowClick = (event) => {
-   event.currentTarget.classList.toggle("following");
+   event.currentTarget.classList.toggle("followingButton");
   dispatch(userFollowToggle(event.currentTarget.id));
 }
 
@@ -18,14 +18,14 @@ const handleFollowClick = (event) => {
       <img className={style.logoImg} src={logo} alt="Logotype of Go-IT School" />
       <img className={style.questionImg} src={pictureQuestion}
         alt="a question and an answer"
-        width="308px" />
+         />
       <div className={style.userAvatar}>
         <img src={avatar} alt="User Avatar" />
       </div>
       <span className={style.avatarLine}></span>
-      <h3 className={style.username}>{username}</h3>
-      <p className={style.followersCount}>{new Intl.NumberFormat('en-US').format(followers)} followers</p>
-      <button id={id} className={style.followButton} onClick={handleFollowClick}>
+      <span className={style.tweets}>{new Intl.NumberFormat('en-US').format(tweets)} tweets</span>
+      <span className={style.followersCount}>{new Intl.NumberFormat('en-US').format(followers)} followers</span>
+      <button id={id} className={status==="follow"? style.followButton : style.followingButton} onClick={handleFollowClick}>
         {status==="follow" ? 'Follow': 'Following'}
       </button>
     </div>
